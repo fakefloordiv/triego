b837f241d38f7030ddceeb6540e00ab785e11763
a very fake floordiv <real.floordiv@gmail.com>
2022-05-25 13:53:50 +0200

    now building now a tree, but some branches that may be switched

--- builder.go
+++ builder.go
@@ -1,18 +1,23 @@
 package prefixtree
 
-const (
-	head = byte(0)
-	tail = byte(1)
-)
+type Branch []Node
 
 type Node struct {
-	char   byte
-	leaves []Node
+	char     byte
+	variants []Branch
 }
 
-func BuildTree(branches [][]byte) Node {
+func (n Node) GetChar() byte {
+	return n.char
+}
+
+func (n Node) GetVariants() []Branch {
+	return n.variants
+}
+
+func BuildTree(branches [][]byte) Branch {
 	longestBranchIndex := longestArray(branches)
-	masterBranch := buildMasterBranch(branches[longestBranchIndex])
+	masterBranch := buildBranch(branches[longestBranchIndex])
 	otherBranches := append(branches[:longestBranchIndex], branches[longestBranchIndex+1:]...)
 
 	for _, branch := range otherBranches {
@@ -22,11 +27,11 @@ func BuildTree(branches [][]byte) Node {
 	return masterBranch
 }
 
-func InsertOne(base Node, branch []byte) Node {
+func InsertOne(base Branch, branch []byte) Branch {
 	return addBranch(base, branch)
 }
 
-func InsertMany(base Node, branches [][]byte) Node {
+func InsertMany(base Branch, branches [][]byte) Branch {
 	for _, branch := range branches {
 		base = addBranch(base, branch)
 	}
@@ -34,94 +39,76 @@ func InsertMany(base Node, branches [][]byte) Node {
 	return base
 }
 
-func newNode(char byte) *Node {
-	return &Node{
-		char:   char,
-		leaves: make([]Node, 0, 1),
-	}
-}
-
-func newTailNode() *Node {
-	return &Node{
-		char:   tail,
-		leaves: nil,
-	}
-}
-
-func (l Node) IsTail() bool {
-	return getNode(l.leaves, tail) != nil
-}
-
 /*
 buildMasterBranch builds a root Node from zero
 */
-func buildMasterBranch(branch []byte) (node Node) {
+func buildBranch(branch []byte) Branch {
 	if len(branch) == 0 {
-		return node
+		return Branch{}
 	}
 
-	rootNode := &Node{
-		char:   head,
-		leaves: make([]Node, 0, 1),
+	masterBranch := make([]Node, len(branch))
+
+	for index, char := range branch {
+		masterBranch[index].char = char
 	}
 
-	currentNode := rootNode
+	// long line as hell, but the sense is to add a nil to the
+	// last node as nil is an indicator that previous node may
+	// be the last one
+	masterBranch[len(masterBranch)-1].variants = append(masterBranch[len(masterBranch)-1].variants, nil)
 
-	for _, char := range branch {
-		node = *newNode(char)
-		currentNode.leaves = append(currentNode.leaves, node)
-		currentNode = &currentNode.leaves[len(currentNode.leaves)-1]
-	}
-
-	currentNode.leaves = append(currentNode.leaves, *newTailNode())
-
-	return *rootNode
+	return masterBranch
 }
 
-func addBranch(base Node, branch []byte) Node {
-	commonPrefixOffset := -1
-	currentNode := &base
+func addBranch(base Branch, newRawBranch []byte) Branch {
+	node, prefix, err := walkByTree(base, newRawBranch)
 
-	for i, char := range branch {
-		Node := getNode(currentNode.leaves, char)
+	if !err {
+		// there are 2 cases that may trigger this:
+		// 1) new branch is a duplicate and trie already has it
+		// 2) new branch is a shorter version of already existing branch
+		// So in second case, we need to add a tail marker
 
-		if Node == nil {
-			commonPrefixOffset = i
-			break
-		}
-
-		currentNode = Node
-	}
-
-	if commonPrefixOffset == -1 {
-		if getNode(currentNode.leaves, tail) == nil {
-			currentNode.leaves = append(currentNode.leaves, *newTailNode())
-		}
+		node.variants = append(node.variants, nil)
 
 		return base
 	}
 
-	for _, char := range branch[commonPrefixOffset:] {
-		Node := *newNode(char)
-		currentNode.leaves = append(currentNode.leaves, Node)
-		currentNode = &currentNode.leaves[len(currentNode.leaves)-1]
-	}
-
-	currentNode.leaves = append(currentNode.leaves, *newTailNode())
+	newBranch := buildBranch(newRawBranch[prefix:])
+	node.variants = append(node.variants, newBranch)
 
 	return base
 }
 
-func getNode(leaves []Node, char byte) *Node {
-	for i := 0; i < len(leaves); i++ {
-		if leaves[i].char != char {
-			continue
+/*
+walkByTree walks by a tree, but used for internals. For example, building
+a tree
+*/
+func walkByTree(root Branch, data []byte) (node *Node, n int, err bool) {
+	var (
+		index  int
+		branch = root
+	)
+
+	for _, char := range data {
+		node = &branch[index]
+
+		if node.char != char {
+			index = 1
+			branch = getBranch(node.variants, char)
+
+			if branch == nil {
+				return node, n, true
+			}
+		} else {
+			index++
 		}
 
-		return &leaves[i]
+		n++
 	}
 
-	return nil
+	return &branch[index-1], n, false
 }
 
 func longestArray(arrays [][]byte) (index int) {
