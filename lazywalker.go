7c2fb42745c05c13322f478ed7e73e95ffc99b24
a very fake floordiv <real.floordiv@gmail.com>
2022-05-25 13:54:42 +0200

    renamed leaf to Node everywhere, changed algo of walking by a trie

--- lazywalker.go
+++ lazywalker.go
@@ -1,29 +1,71 @@
 package prefixtree
 
 type Walker interface {
-	Walk(data []byte) (leaf Node, err bool)
+	Walk(data []byte) (node Node, err bool)
 }
 
 type walker struct {
-	currentNode Node
+	currentBranch Branch
+	currentIndex  int
 }
 
-func NewWalker(root Node) Walker {
+func NewWalker(root Branch) Walker {
 	return &walker{
-		currentNode: root,
+		currentBranch: root,
 	}
 }
 
+/*
+Walk walks lazily (if no error was returned before, you can continue
+matching a string by calling this function one more time with a next
+part of data)
+
+Returned node is the last one node. In case of failure, it is unmatched
+node (that wanted to be, but dreams aren't always real)
+*/
 func (w *walker) Walk(data []byte) (node Node, err bool) {
 	for _, char := range data {
-		childNode := getNode(w.currentNode.leaves, char)
+		node = w.currentBranch[w.currentIndex]
 
-		if childNode == nil {
-			return w.currentNode, true
+		if node.char != char {
+			newBranch := getBranch(node.variants, char)
+
+			if newBranch == nil {
+				return node, true
+			}
+
+			w.currentBranch = newBranch
+			w.currentIndex = 1
+		} else {
+			w.currentIndex++
 		}
-
-		w.currentNode = *childNode
 	}
 
-	return w.currentNode, false
+	return w.currentBranch[w.currentIndex-1], false
+}
+
+func IsTail(node Node) bool {
+	for _, branch := range node.variants {
+		if branch == nil {
+			return true
+		}
+	}
+
+	return false
+}
+
+/*
+getBranch returns a branch a first node of which one matches
+a char we need. If nil is returned, this means that we could
+not find a corresponding branch (tailing branch cannot be
+returned)
+*/
+func getBranch(branches []Branch, char byte) Branch {
+	for _, branch := range branches {
+		if branch[0].char == char {
+			return branch
+		}
+	}
+
+	return nil
 }
